4318c4f022a36f720445b81065e0ac4ab408501c
andriyav <aandrusy@softserveinc.com>
2024-10-02 22:40:59 +0300

    Chrome_driver correction

--- tests/test_runners.py
+++ tests/test_runners.py
@@ -21,9 +21,12 @@ class BaseTestRunner(unittest.TestCase):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
         # chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-popup-blocking")
         # chrome_options.add_argument('--disable-web-security')
         # chrome_options.add_argument('--disable-extensions')
-        chrome_options.add_argument('--window-size=1920x1080')
+        # chrome_options.add_argument('--window-size=1920x1080')
 
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
