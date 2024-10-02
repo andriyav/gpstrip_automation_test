85f52107139c026904a70c5db2fb579d691c263d
andriyav <aandrusy@softserveinc.com>
2024-10-02 23:16:32 +0300

    Chrome_driver correction

--- tests/test_runners.py
+++ tests/test_runners.py
@@ -29,8 +29,11 @@ class BaseTestRunner(unittest.TestCase):
         # chrome_options.add_argument('--window-size=1920x1080')
 
         chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument('--window-size=1920x1080')
+        # chrome_options.add_argument('--window-size=1920x1080')
+        # chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
+        # driver.set_window_size(1920, 1080)
 
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
