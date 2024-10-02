9d26fbb3a2bc2c732f57947f6159669804c94299
andriyav <aandrusy@softserveinc.com>
2024-10-02 23:31:41 +0300

    Chrome_driver correction

--- tests/test_runners.py
+++ tests/test_runners.py
@@ -28,7 +28,7 @@ class BaseTestRunner(unittest.TestCase):
         # chrome_options.add_argument('--disable-extensions')
         # chrome_options.add_argument('--window-size=1920x1080')
 
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument('--window-size=1920x1080')
         # chrome_options.add_argument('--window-size=1920x1080')
         # chrome_options.add_argument("--no-sandbox")
